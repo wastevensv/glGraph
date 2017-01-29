2ed9c7d77dfe7d081fb09c830f105bfc58baa0a4
William Stevens <contact@wastevensv.com>
2017-01-29 13:28:24 -0500

    Now in 3D.

--- main.cpp
+++ main.cpp
@@ -33,6 +33,8 @@ int main()
 
     // ----------------------------- RESOURCES ----------------------------- //
 
+    glEnable(GL_DEPTH_TEST);
+
     // Create Vertex Array Object
     GLuint vao;
     glGenVertexArrays(1, &vao);
@@ -43,28 +45,33 @@ int main()
     glGenBuffers(1, &vbo);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
 
-    vector<Vert<float, 5>> vertices = {
-        { 0.5f, -0.5f, 1.0f, 0.0f, 0.0f}, // Vertex 1: Red
-        {-0.5f,  0.5f, 0.0f, 1.0f, 0.0f}, // Vertex 2: Green
-        { 0.5f,  0.5f, 0.0f, 1.0f, 0.0f}, // Vertex 3: Green
-        { 0.5f, -0.5f, 1.0f, 0.0f, 0.0f}, // Vertex 1: Red
-        {-0.5f,  0.5f, 0.0f, 1.0f, 0.0f}, // Vertex 2: Green
-        {-0.5f, -0.5f, 0.0f, 0.0f, 1.0f}, // Vertex 3: Blue
+    vector<Vert<float, 6>> vertices = {
+        // X      Y     Z     R     G     B
+        { 0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f}, // Vertex 1: Red
+        {-0.5f,  0.5f, 0.0f, 0.0f, 1.0f, 0.0f}, // Vertex 2: Green
+        { 0.5f,  0.5f, 0.0f, 0.0f, 1.0f, 0.0f}, // Vertex 3: Green
+        { 0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f}, // Vertex 1: Red
+        {-0.5f,  0.5f, 0.0f, 0.0f, 1.0f, 0.0f}, // Vertex 2: Green
+        {-0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f}, // Vertex 3: Blue
     };
 
-    glBufferData(GL_ARRAY_BUFFER, vertices.size()*sizeof(Vert<float, 8>),
+    glBufferData(GL_ARRAY_BUFFER, vertices.size()*sizeof(Vert<float, 6>),
            &vertices[0], GL_STATIC_DRAW);
 
 
     // Create the vertex shader
     const char* vertexSource = GLSL(
-        in vec2 position;
+        in vec3 position;
         in vec3 color;
         out vec3 Color;
 
+        uniform mat4 model;
+        uniform mat4 view;
+        uniform mat4 proj;
+
         void main() {
             Color = color;
-            gl_Position = vec4(position, 0.0, 1.0);
+            gl_Position = proj * view * model * vec4(position, 1.0);
         }
     );
 
@@ -87,27 +94,44 @@ int main()
     // Specify the layout of the vertex data
     GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
     glEnableVertexAttribArray(posAttrib);
-    glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE,
-        5*sizeof(float), 0);
+    glVertexAttribPointer(posAttrib, 3, GL_FLOAT, GL_FALSE,
+        6*sizeof(float), 0);
 
     GLint colAttrib = glGetAttribLocation(shaderProgram, "color");
     glEnableVertexAttribArray(colAttrib);
-    glVertexAttribPointer(colAttrib, 3, GL_FLOAT, GL_FALSE, 5*sizeof(float),
-        (void*)(2*sizeof(float)));
+    glVertexAttribPointer(colAttrib, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float),
+        (void*)(3*sizeof(float)));
 
     // ---------------------------- RENDERING ------------------------------ //
+
+    // Setup Projection
+    glm::mat4 proj =
+        glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.5f, 20.0f);
+    glm::mat4 model = glm::mat4();
+    glm::mat4 view = glm::lookAt(
+            glm::vec3( 2.0f,  2.0f, 1.0f),
+            glm::vec3( 0.0f,  0.0f, 0.0f),
+            glm::vec3( 0.0f,  0.0f, 1.0f)
+        );
+
+    glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "view"),
+            1, GL_FALSE, glm::value_ptr(view));
+    glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "proj"),
+            1, GL_FALSE, glm::value_ptr(proj));
+    glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "model"),
+            1, GL_FALSE, glm::value_ptr(model));
 
     while(!glfwWindowShouldClose(window))
     {
         // Calculate new triangle alpha for current frame
         GLint uniColor = glGetUniformLocation(shaderProgram, "vertexColor");
         float time = (float)glfwGetTime();
-        float adjfactor = sin(time) - 0.5f;
+        float adjfactor = (sin(time)/2) + 0.5f;
         glUniform3f(uniColor, adjfactor, adjfactor, adjfactor);
 
         // Clear the screen to black
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         // Draw a triangle from the 3 vertices
         glDrawArrays(GL_TRIANGLES, 0, vertices.size());
--- verts.hpp
+++ verts.hpp
@@ -12,20 +12,6 @@ struct Vert {
     T data[N];
 };
 
-template<>
-struct Vert<float, 3> {
-  public:
-    float &operator[](std::size_t idx) { return data[idx]; }
-    float data[3] = {0.0f, 0.0f, 0.0f};
-};
-
-template<>
-struct Vert<float, 6> {
-  public:
-    float &operator[](std::size_t idx) { return data[idx]; }
-    float data[6] = {0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f};
-};
-
 template<typename T, unsigned int N>
 std::ostream &operator<< (std::ostream &os, const Vert<T,N> &m) {
   for (int i = 0; i < N-1; ++i) {
